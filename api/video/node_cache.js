4f44a1824a77aa7aeb2eb118f776092f0b06a571
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 14:46:28 -0700

    Update node_cache.js

--- api/video/node_cache.js
+++ api/video/node_cache.js
@@ -1,4 +1,4 @@
-
+if (!req.query['type']) { res.send('Missing type'); return true; }
 if (req.query['type'].indexOf(['video', 'section','image'])) { res.send('type error '); return true; }
 res.send('== ' + req.query['type'] + ' ==');
 
