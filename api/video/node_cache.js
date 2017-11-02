4ac47ce5fc3c646ecc95ab9fe263e458057bddf7
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 15:17:18 -0700

    Update node_cache.js

--- api/video/node_cache.js
+++ api/video/node_cache.js
@@ -1,6 +1,8 @@
 if (['video', 'section','image'].indexOf(req.query['type']) === -1) { res.send('type error '); return true; }
 if (isNaN(req.query['vid']) || !parseInt(req.query['vid'])) { res.send('wrong vid'); return true; }
 
+var vid = req.query['vid'];
+
 var mnt_folder = '/var/shusiou-video/videos/';
 var target_folder = mnt_folder +  + vid + '/' + req.query['type'] + '/';
 
