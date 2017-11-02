047bdbbc038533fa2d9c7fe01d9b9f3defa343ad
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 14:30:48 -0700

    Update node_cache.js

--- api/video/node_cache.js
+++ api/video/node_cache.js
@@ -1,3 +1,5 @@
+res.send('==niu==');
+return true;
 if (!req.query['vid']) {
 	res.send('vid error ');
 	return true;
@@ -5,63 +7,5 @@ if (!req.query['vid']) {
 var channel = (req.query['channel'])?parseInt(req.query['channel']):0;
 
 var fn = '/tmp/video_'+req.query['vid']+'.mp4';
-
-function streamVideo(req, res) {
-	pkg.fs.stat(fn, function(err, data) {
-	    if (err) {
-	      res.send('Video does not exist');
-	    } else {	
-	      var total = data.size;
-	      var range = req.headers.range;
-	      if (range) {
-			var parts = range.replace(/bytes=/, "").split("-");
-			var partialstart = parts[0]; var partialend;
-			  partialend =  parts[1];
-			var start = parseInt(partialstart, 10);
-			var end = partialend ? parseInt(partialend, 10) : total-1;
-			var chunksize = (end-start)+1;
-			var file = pkg.fs.createReadStream(fn, {start:start, end:end});
-
-			res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
-				'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
-		       file.pipe(res);
-		} else {
-			res.send('Need streaming player');
-		}
-	    }
-	});	
-}
-function pull_stream(req, res) {
-	var request = require(env.root_path + '/package/request/node_modules/request');
-	var file = pkg.fs.createWriteStream(fn);
-	var http = require('http');
-	var tm =  new Date().getTime();
-	var request = http.get('http://shusiou.com/api/video/test_pipe.api?vid='+req.query['vid'], function(response) {
-		response.pipe(file);
-		response.on('end', function() {
-			 streamVideo(req, res);
-		});
-	});
-}	
-
-pkg.fs.stat(fn, function(err, data) {
-    if (err) {
-	pull_stream(req, res);
-    } else {
-	  var d = parseInt(new Date().getTime() * 0.001) - parseInt(data.ctimeMs * 0.001);  
-	  if (!data.size && d < 30) {
-		 if (channel > 3) {
-			 res.send('Error! timeout');
-		 } else {
-			 setTimeout(function() {
-				res.redirect(req.url + '&channel=' + (channel+1));
-			 }, Math.floor(Math.random() * (1000)));
-		 }	 
-	  } else {
-	  	streamVideo(req, res);
-	  }		  
-    }
-});	
-
 
 
