d9e3942e01aea6d50ab26c1cee8da3c07b7d4cfe
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 08:34:09 -0700

    Update node_play_video.js

--- api/video/node_play_video.js
+++ api/video/node_play_video.js
@@ -39,7 +39,23 @@ var request = http.get('http://shusiou.com/api/video/test_pipe.api?vid='+req.que
 		    if (err) {
 		      res.send('Video does not exist');
 		    } else {
-			 streamVideo(fn, req, res);
+			      var total = data.size;
+			      var range = req.headers.range;
+			      if (range) {
+					var parts = range.replace(/bytes=/, "").split("-");
+					var partialstart = parts[0]; var partialend;
+					  partialend =  parts[1];
+					var start = parseInt(partialstart, 10);
+					var end = partialend ? parseInt(partialend, 10) : total-1;
+					var chunksize = (end-start)+1;
+					var file = pkg.fs.createReadStream(fn, {start:start, end:end});
+
+					res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
+						'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+				       file.pipe(res);
+				} else {
+					res.send('Need streaming player');
+				}
 			}
 		});
 	});
