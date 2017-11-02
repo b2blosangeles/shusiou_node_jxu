581a4ef9e58e14e045a7ac68df5056c470eb3188
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 07:54:46 -0700

    Update node_play_video.js

--- api/video/node_play_video.js
+++ api/video/node_play_video.js
@@ -29,7 +29,7 @@ function streamVideo() {
 
 
 var request = require(env.root_path + '/package/request/node_modules/request');
-var file = pkg.fs.createWriteStream('/tmp/video_a.mp4');
+var file = pkg.fs.createWriteStream(fn);
 var http = require('http');
 var tm =  new Date().getTime();
 var request = http.get('http://shusiou.com/api/video/test_pipe.api?vid='+req.query['vid'], function(response) {
