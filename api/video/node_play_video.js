45dffd8e88a36b0b2053fbe25f30847380919db1
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 09:38:58 -0700

    Update node_play_video.js

--- api/video/node_play_video.js
+++ api/video/node_play_video.js
@@ -4,24 +4,30 @@ if (!req.query['vid']) {
 }
 var fn = '/tmp/video_'+req.query['vid']+'.mp4';
 
-function streamVideo(req, res, data) {
-      var total = data.size;
-      var range = req.headers.range;
-      if (range) {
-		var parts = range.replace(/bytes=/, "").split("-");
-		var partialstart = parts[0]; var partialend;
-		  partialend =  parts[1];
-		var start = parseInt(partialstart, 10);
-		var end = partialend ? parseInt(partialend, 10) : total-1;
-		var chunksize = (end-start)+1;
-		var file = pkg.fs.createReadStream(fn, {start:start, end:end});
+function streamVideo(req, res) {
+	pkg.fs.stat(fn, function(err, data) {
+	    if (err) {
+	      res.send('Video does not exist');
+	    } else {	
+	      var total = data.size;
+	      var range = req.headers.range;
+	      if (range) {
+			var parts = range.replace(/bytes=/, "").split("-");
+			var partialstart = parts[0]; var partialend;
+			  partialend =  parts[1];
+			var start = parseInt(partialstart, 10);
+			var end = partialend ? parseInt(partialend, 10) : total-1;
+			var chunksize = (end-start)+1;
+			var file = pkg.fs.createReadStream(fn, {start:start, end:end});
 
-		res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
-			'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
-	       file.pipe(res);
-	} else {
-		res.send('Need streaming player');
-	}
+			res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
+				'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+		       file.pipe(res);
+		} else {
+			res.send('Need streaming player');
+		}
+	    }
+	});	
 }
 
 
@@ -32,34 +38,9 @@ var request = require(env.root_path + '/package/request/node_modules/request');
 var file = pkg.fs.createWriteStream(fn);
 var http = require('http');
 var tm =  new Date().getTime();
-var request = http.get('http://shusiou.com/api/video/test_pipe.api?vid='+req.query['vid'], function(response) {
+var request = http.get('http://shusiou.com/api/video/test_pipe.api?vid=1&'+req.query['vid'], function(response) {
 	response.pipe(file);
 	response.on('end', function() {
-		pkg.fs.stat(fn, function(err, data) {
-		    if (err) {
-		      res.send('Video does not exist');
-		    } else {
-			    streamVideo(req, res, data);
-			    /*
-			      var total = data.size;
-			      var range = req.headers.range;
-			      if (range) {
-					var parts = range.replace(/bytes=/, "").split("-");
-					var partialstart = parts[0]; var partialend;
-					  partialend =  parts[1];
-					var start = parseInt(partialstart, 10);
-					var end = partialend ? parseInt(partialend, 10) : total-1;
-					var chunksize = (end-start)+1;
-					var file = pkg.fs.createReadStream(fn, {start:start, end:end});
-
-					res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
-						'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
-				       file.pipe(res);
-				} else {
-					res.send('Need streaming player');
-				}
-				*/
-			}
-		});
+		 streamVideo(req, res);
 	});
 });
