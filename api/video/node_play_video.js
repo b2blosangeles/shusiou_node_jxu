6db0e0815803920c9f9d6a54a7f38c7fd1c5356c
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 07:46:05 -0700

    Update node_play_video.js

--- api/video/node_play_video.js
+++ api/video/node_play_video.js
@@ -2,8 +2,37 @@ if (!req.query['vid']) {
 	res.send('vid error ');
 	return true;
 }
+var fn = '/tmp/video_'+req.query['vid']+'.mp4';
 
-var fn = '/tmp/video_a.mp4';
+function streamVideo() {
+      var total = data.size;
+      var range = req.headers.range;
+      if (range) {
+		var parts = range.replace(/bytes=/, "").split("-");
+		var partialstart = parts[0]; var partialend;
+		  partialend =  parts[1];
+		var start = parseInt(partialstart, 10);
+		var end = partialend ? parseInt(partialend, 10) : total-1;
+		var chunksize = (end-start)+1;
+		var file = pkg.fs.createReadStream(fn, {start:start, end:end});
+
+		res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
+			'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+	       file.pipe(res);
+	} else {
+		res.send('Need streaming player');
+	}
+}
+
+
+pkg.fs.stat(fn, function(err, data) {
+    if (err) {
+      res.send('Video does not exist');
+    } else {
+	 streamVideo()
+	}
+});
+
 
 var request = require(env.root_path + '/package/request/node_modules/request');
 var file = pkg.fs.createWriteStream('/tmp/video_a.mp4');
