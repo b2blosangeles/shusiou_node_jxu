9fc4fb1af41b75c125ca95d17ec36a5d98c8a666
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 15:06:40 -0700

    Update node_cache.js

--- api/video/node_cache.js
+++ api/video/node_cache.js
@@ -1,5 +1,5 @@
 if (['video', 'section','image'].indexOf(req.query['type']) === -1) { res.send('type error '); return true; }
-if (!parseInt(req.query['vid'])) { res.send('missing vid'); return true; }
+if (isNaN(req.query['vid']) || !parseInt(req.query['vid'])) { res.send('wrong vid'); return true; }
 res.send('== ' + req.query['type'] + ' ==');
 
 
