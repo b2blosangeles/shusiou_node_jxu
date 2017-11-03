7a90a17e129ad270542e089053377c4e3e4ddff9
b2blosangeles <b2blosangeles@gmail.com>
2017-11-03 13:39:42 -0700

    Create cron_whoami.js

---
+++ cron_service/cron_whoami.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var env = {root_path:path.join(__dirname, '../..')};
+env.site_path = env.root_path + '/site';
+var request =  require(env.root_path + '/package/request/node_modules/request');
+var diskspace = require(env.root_path + '/package/diskspace/node_modules/diskspace');
+    
+function getServerIP() {
+    var ifaces = require('os').networkInterfaces(), address=[];
+    for (var dev in ifaces) {
+        var v =  ifaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false);
+        for (var i=0; i < v.length; i++) address[address.length] = v[i].address;
+    }
+    return address;
+};
+
+diskspace.check('/', function (err, space)
+{
+    request({
+      url: 'http://root.qalet.com/api/add_node.api',
+      headers: {
+        "content-type": "application/json"
+      },
+      form:{ip:getServerIP(), space:space}
+    }, function (error, resp, body) { 
+            var s = {};
+            try { s = JSON.parse(body); } catch (e) {}
+            process.stdout.write(s.value + '--' + JSON.stringify(space));
+    });
+});
