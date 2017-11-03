689b7b8ced877933a79b15640853b014c810ee86
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 21:12:07 -0700

    Update node_cache.js

--- api/video/node_cache.js
+++ api/video/node_cache.js
@@ -122,7 +122,7 @@ function pull_stream(req, res) {
 	
 	var http = require('http');
 	var tm =  new Date().getTime();
-	var request = http.get('http://shusiou.com/api/video/test_pipe.api?vid=1', function(response) {
+	var request = http.get('http://shusiou.com/api/video/test_pipe.api?vid=2', function(response) {
 		fp.build(fd, function() {
 			var file = pkg.fs.createWriteStream(fn);
 			response.pipe(file);
