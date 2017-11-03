e56e3b197f38c90c481819a43692684affb22765
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 20:28:31 -0700

    Update node_cache.js

--- api/video/node_cache.js
+++ api/video/node_cache.js
@@ -126,9 +126,10 @@ function pull_stream(req, res) {
 		var file = pkg.fs.createWriteStream(fn);
 		var fp = new folderP();
 		fp.build(fd, function() {
-			response.pipe(file);
+			
 			res.send('--nn2--');
 				return true;
+			response.pipe(file);
 			response.on('end', function() {
 				res.send('--nn3--');
 				return true;
