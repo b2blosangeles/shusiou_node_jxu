20481930730588d2526693a3f148bc6ce27d837b
b2blosangeles <b2blosangeles@gmail.com>
2017-11-05 06:37:34 -0800

    Update node_audit.js

--- api/node_audit.js
+++ api/node_audit.js
@@ -2,6 +2,8 @@ var opt = req.query['opt'];
 
 switch(opt) {
 	case 'status':
+		var CP = new pkg.crowdProcess();
+		var _f = {};
 		    pkg.fs.readFile('/var/.qalet_whoami.data', 'utf8', function(err,data) {
 			if ((err) || !data) {
 				res.send('error');		
