5aca30ec12e8246feabad254772fdf47a06206de
b2blosangeles <b2blosangeles@gmail.com>
2017-11-05 06:36:13 -0800

    Update node_audit.js

--- api/node_audit.js
+++ api/node_audit.js
@@ -2,7 +2,13 @@ var opt = req.query['opt'];
 
 switch(opt) {
 	case 'status':
-		res.send('node_audit.js -- status');	
+		    pkg.fs.readFile('/var/.qalet_whoami.data', 'utf8', function(err,data) {
+			if ((err) || !data) {
+				res.send('error');		
+			} else {
+				res.send(data);
+			}
+		    });			
 		break;
 	default:
 		res.send({status:'error', message:'Wrong opt value!'});
