f94859c11db1dc70d40276812d08a8e4fd355790
b2blosangeles <b2blosangeles@gmail.com>
2017-11-05 06:39:55 -0800

    Update node_audit.js

--- api/node_audit.js
+++ api/node_audit.js
@@ -4,13 +4,23 @@ switch(opt) {
 	case 'status':
 		var CP = new pkg.crowdProcess();
 		var _f = {};
+		_f['P0'] = function(cbk) {
 		    pkg.fs.readFile('/var/.qalet_whoami.data', 'utf8', function(err,data) {
 			if ((err) || !data) {
 				res.send('error');		
 			} else {
 				res.send(data);
 			}
-		    });			
+		    });
+		};
+		CP.serial(
+			_f,
+			function(data) {
+				res.send({status:'success',ip:data.results.P0});
+			},
+			1000
+		);		
+		
 		break;
 	default:
 		res.send({status:'error', message:'Wrong opt value!'});
