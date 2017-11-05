5718f78a3dc93d1aa3192eda363d776bedae0e8f
b2blosangeles <b2blosangeles@gmail.com>
2017-11-05 08:01:27 -0800

    Update node_audit.js

--- api/node_audit.js
+++ api/node_audit.js
@@ -1,5 +1,4 @@
 var opt = req.query['opt'];
-delete require.cache[env.site_path + '/api/cfg/channel.json'];
 var channel = require(env.site_path + '/api/cfg/channel.json');
 switch(opt) {
 	case 'status':
