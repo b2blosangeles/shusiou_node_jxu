80c904db4f6c0e6a076222214c7604718884fa96
b2blosangeles <b2blosangeles@gmail.com>
2017-11-06 07:59:01 -0800

    Update node_audit.js

--- api/node_audit.js
+++ api/node_audit.js
@@ -45,7 +45,7 @@ switch(opt) {
 					pkg.fs.stat(fn, function(err, st) {
 						var v = {};
 						try { v = JSON.parse(st); } catch (e) {}
-						cbk(v.size);
+						cbk(st);
 					});
 				}
 			})(i);
