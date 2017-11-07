f0d375b25196cddb34475c67b04d6008bdc98d09
b2blosangeles <b2blosangeles@gmail.com>
2017-11-06 16:11:03 -0800

    Update node_audit.js

--- api/node_audit.js
+++ api/node_audit.js
@@ -69,7 +69,7 @@ switch(opt) {
 				var v1 = [];
 				for (var i = 0; i < list.length; i++) {
 					//if (CP.data['V_' + i]) {
-						v1[v1.length] = CP.data['V_' + i];
+						v1[v1.length] = CP.data['V_'+list[i]];
 					//}
 				}
 				var diff = CP.data.I0.filter(x => v1.indexOf(x) < 0 );
