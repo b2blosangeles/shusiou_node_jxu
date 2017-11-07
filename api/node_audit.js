1260b3b29df144abc7a65da680e5a0b432ad00b7
b2blosangeles <b2blosangeles@gmail.com>
2017-11-06 16:38:12 -0800

    Update node_audit.js

--- api/node_audit.js
+++ api/node_audit.js
@@ -62,6 +62,7 @@ switch(opt) {
 		CP.parallel(
 			_f,
 			function(data) {
+				/*
 				var v1 = [];
 				for (var i = 0; i < list.length; i++) {
 					if (CP.data['V_' + list[i]]) {
@@ -69,6 +70,7 @@ switch(opt) {
 					}
 				}
 				var diff = CP.data.I0.filter(x => v1.indexOf(x) < 0 );
+				*/
 				// res.send({diff:diff, v1:v1});
 				res.send(data.result);
 			},
