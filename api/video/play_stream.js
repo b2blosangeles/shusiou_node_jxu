e75f6695ce8341090ae938c3a58efcad38f3f620
b2blosangeles <b2blosangeles@gmail.com>
2017-11-09 18:28:51 -0800

    Update play_stream.js

--- api/video/play_stream.js
+++ api/video/play_stream.js
@@ -1,1 +1,9 @@
-res.send(env);
+function write404(msg) {
+	res.writeHead(404);
+	res.write(msg);
+	res.end();	
+}
+
+var type= req.query['type'], vid = req.query['vid'], server = req.query['server'];
+if (!type || !vid) {  write404('vid or type error '); return true; }
+res.send(req.url);
