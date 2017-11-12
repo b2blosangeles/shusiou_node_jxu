b7c92c3d72ed47734f86a609fc10a57c035b5131
b2blosangeles <b2blosangeles@gmail.com>
2017-11-11 21:34:45 -0800

    Update play_stream.js

--- api/video/play_stream.js
+++ api/video/play_stream.js
@@ -80,7 +80,9 @@ switch(type) {
 		
 		_f['S2'] = function(cbk) {
 			pkg.fs.stat(fn, function(err, stat) {
-				if(!err) { cbk(fn);
+				if(!err) { 
+					cbk(stat.size);
+					// cbk(fn);
 				} else {
 					var request = http.get(url, function(response) {
 						if (response.statusCode == 404 || response.statusCode == 500) {
@@ -152,8 +154,8 @@ switch(type) {
 		_f['S2'] = function(cbk) {
 			pkg.fs.stat(fn, function(err, stat) {
 				if(!err) { 
-					cbk(stat.size);
-					// cbk(fn);
+				
+					cbk(fn);
 				} else {
 					var request = http.get(url + '&cache_only=1', function(response) {
 						if (response.statusCode == 404 || response.statusCode == 500) {
