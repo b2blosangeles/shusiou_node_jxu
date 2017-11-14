c92f657d1000740740f548d825e6e55ee6230b7f
b2blosangeles <b2blosangeles@gmail.com>
2017-11-13 16:24:01 -0800

    Update play_stream.js

--- api/video/play_stream.js
+++ api/video/play_stream.js
@@ -268,7 +268,7 @@ switch(type) {
 								'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
 						       file.pipe(res);
 						} else {
-							res.send('Need streaming player');
+							res.send('Need streaming player 2');
 						}
 					}
 				});
