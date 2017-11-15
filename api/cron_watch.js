1bdf083506c4f9c4ec5612323ecabe3169ce7250
b2blosangeles <b2blosangeles@gmail.com>
2017-11-15 08:11:22 -0800

    Update cron_watch.js

--- api/cron_watch.js
+++ api/cron_watch.js
@@ -8,19 +8,13 @@ pkg.fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
             res.send('skip!');
         } else if ((watch.mark)) {
             var d = new Date().getTime() - new Date(watch.mark).getTime();
-       //   res.send(d + '');
-       //   return true;
-
             if (d > 180000) {
-                res.send('need boot');
-              /*
                 var watch0 = {start:new Date(), mark:new Date()};
                 pkg.fs.writeFile('/var/.qalet_cron_watch.data', JSON.stringify(watch0), function (err) {
                     pkg.exec('shutdown -r +1', function(error, stdout, stderr) {
                       res.send('Server will be reboot in 1 minute!');
                     });             
                 });
-               */
             } else {
                 res.send('normal');
             }
