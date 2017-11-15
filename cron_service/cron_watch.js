9cc8a21e8f2f3a9cb3c97b97909ddf13df476a35
b2blosangeles <b2blosangeles@gmail.com>
2017-11-15 08:04:28 -0800

    Update cron_watch.js

--- cron_service/cron_watch.js
+++ cron_service/cron_watch.js
@@ -4,49 +4,28 @@ env.site_path = env.root_path + '/site';
 var request =  require(env.root_path + '/package/request/node_modules/request');
 var fs = require('fs');
 
-var watch0 = {start:new Date(), prev:new Date(), now:new Date()};
+var watch0 = {start:new Date(), mark:new Date()};
 fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
   if (err){
       fs.writeFile('/var/.qalet_cron_watch.data', JSON.stringify(watch0), function (err) {});
   } else {
     var watch = {};
     try { watch = JSON.parse(data);} catch (e) {}
-    
-    if ((watch.prev) && (watch.now)) {
+    if (watch.mark)  {
       delete watch.start;
-      watch.prev = watch.now;
-      watch.now = new Date();
+      watch.mark = new Date();
       fs.writeFile('/var/.qalet_cron_watch.data', JSON.stringify(watch), function (err) {
           console.log(watch);
       });
     } 
   }
 });	 
-/*
-var channel = require(env.site_path + '/api/cfg/channel.json');
 
-function getIP() {
-    var ifaces = require('os').networkInterfaces(), address=[];
-    for (var dev in ifaces) {
-        var v =  ifaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false);
-        for (var i=0; i < v.length; i++) address[address.length] = v[i].address;
-    }
-    return address;
-};
 request({
     url: 'http://root.qalet.com/api/cron_watch.api',
     headers: {
       "content-type": "application/json"
     },
-    form:{ip:getServerIP(), space:space}
+    form:{}
   }, function (error, resp, body) { 
-          var s = {};
-          try { s = JSON.parse(body); } catch (e) {}
-          process.stdout.write(s.value + '--' + JSON.stringify(space));
   });
-
-fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
-	if ((data) && ips.indexOf(data) != -1)  cbk(data);
-	else { cbk(false); CP.exit = 1; }
-});	 
-*/
