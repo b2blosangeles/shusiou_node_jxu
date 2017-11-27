0051b56c043385e4b412e2b704e9063dd46fb3f5
b2blosangeles <b2blosangeles@gmail.com>
2017-11-27 13:21:33 -0800

    Update cron_whoami.js

--- cron_service/cron_whoami.js
+++ cron_service/cron_whoami.js
@@ -1,3 +1,5 @@
+/* --- code for who am I watch ---*/
+
 var path = require('path');
 var env = {root_path:path.join(__dirname, '../..')};
 env.site_path = env.root_path + '/site';
@@ -33,3 +35,47 @@ diskspace.check('/', function (err, space) {
             process.stdout.write(s.value + '--' + JSON.stringify(space));
     });
 });
+
+/* --- code for cron watch ---*/
+(function(){
+      var path = require('path');
+      var env = {root_path:path.join(__dirname, '../..')};
+      env.site_path = env.root_path + '/site';
+      var request =  require(env.root_path + '/package/request/node_modules/request');
+      var fs = require('fs');
+
+      var watch0 = {start:new Date(), mark:new Date()};
+      fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
+        if (err){
+            fs.writeFile('/var/.qalet_cron_watch.data', JSON.stringify(watch0), function (err) {});
+        } else {
+          var watch = {};
+          try { watch = JSON.parse(data);} catch (e) {}
+          if (watch.mark)  {
+            delete watch.start;
+            watch.mark = new Date();
+            fs.writeFile('/var/.qalet_cron_watch.data', JSON.stringify(watch), function (err) {
+                console.log(watch);
+            });
+          } 
+        }
+      });	 
+
+      function randomInt(min,max) {
+          return Math.floor(Math.random()*(max-min+1)+min);
+      }
+      var delay = randomInt(0,300) * 10;
+      setTimeout(
+        function() {
+            request({
+              url: 'http://root.qalet.com/api/cron_watch.api',
+              headers: {
+                "content-type": "application/json"
+              },
+              form:{}
+            }, function (error, resp, body) { 
+              console.log(delay + '--' + body);
+            });
+        }, delay
+      );
+})();
