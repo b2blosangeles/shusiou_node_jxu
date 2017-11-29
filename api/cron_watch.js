51ce39a3770118ff35488bf243c19d54aace7aad
b2blosangeles <b2blosangeles@gmail.com>
2017-11-29 14:14:43 -0800

    Update cron_watch.js

--- api/cron_watch.js
+++ api/cron_watch.js
@@ -24,8 +24,3 @@ pkg.fs.readFile('/var/.qalet_cron_watch.data', 'utf8', function(err,data) {
         }
   }
 });	 
-
-/* ---  git-update ---*/
-pkg.exec('cd ' + env.site_path + '&& git pull', function(error, stdout, stderr) {
-	 res.send(stdout);
-});
