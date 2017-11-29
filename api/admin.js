c3f5563e49d9de7c63a1a3879b031b97d42ff936
b2blosangeles <b2blosangeles@gmail.com>
2017-11-29 14:26:59 -0800

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -14,9 +14,16 @@ switch(req.body['opt']) {
 		
     case 'git_site_pull':
 	pkg.exec('cd ' + env.site_path + '&& git pull', function(error, stdout, stderr) {
-		 res.send('stdout--');
+		 res.send(stdout);
 	});
         break;	
+
+    case 'git_all_pull':
+	var cmd = 'cd ' + env.site_path + '&& git pull && cd ' + env.root_path + '&& git pull'; 	
+	pkg.exec(cmd, function(error, stdout, stderr) {
+		 res.send(stdout);
+	});
+        break;			
 		
     case 'reboot':	
 	pkg.exec('shutdown -r +1', function(error, stdout, stderr) {
