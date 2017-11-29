577418629db1ee45fbaace6a66c1fc8c4be893eb
b2blosangeles <b2blosangeles@gmail.com>
2017-11-29 12:06:27 -0800

    Create admin.js

---
+++ api/admin.js
@@ -0,0 +1,28 @@
+if (!req.body['opt']) {
+	res.send('Missing opt!!');
+	return true;
+}
+switch(req.body['opt']) {
+		
+    case 'git_frame_pull':
+	pkg.exec('cd ' + env.root_path + '&& git pull', function(error, stdout, stderr) {
+		 res.send(stdout);
+	});
+        break;
+		
+    case 'git_site_pull':
+	pkg.exec('cd ' + env.site_path + '&& git pull', function(error, stdout, stderr) {
+		 res.send(stdout);
+	});
+        break;	
+		
+    case 'reboot':	
+	pkg.exec('shutdown -r +1', function(error, stdout, stderr) {
+	 	res.send('Server will be reboot in 1 minute!');
+	});
+	break;
+		
+    default:
+	res.send('Wrong opt parpmeter!!');
+	return true;
+}
