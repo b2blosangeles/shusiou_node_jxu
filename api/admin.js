b7b3c5216a057f9a568ba91627e5b34898fd6d06
b2blosangeles <b2blosangeles@gmail.com>
2017-11-29 13:56:16 -0800

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -5,6 +5,8 @@ if (!req.body['opt']) {
 switch(req.body['opt']) {
 		
     case 'git_frame_pull':
+	res.send('git_frame_pull');
+		return true;
 	pkg.exec('cd ' + env.root_path + '&& git pull', function(error, stdout, stderr) {
 		if (error) res.send('error');
 		else if (stdout) res.send(stdout);
