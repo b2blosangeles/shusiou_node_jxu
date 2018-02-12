abe823189ef172a12293dca59f801de85193d1ca
b2blosangeles <b2blosangeles@gmail.com>
2018-02-12 10:08:52 -0800

    Update ad1.js

--- api/ad1.js
+++ api/ad1.js
@@ -1,4 +1,7 @@
-	pkg.fs.exists('/var/cert/', function(exists) {
+res.send(env);
+return true;
+
+pkg.fs.exists('/var/cert/', function(exists) {
 		let cmd_plus = (exists)?' && cd /var/cert/ && git pull ':'';
 		var cmd = 'cd ' + env.site_path + '&& git pull && cd ' + env.root_path + '&& git pull' + cmd_plus; 	
 		pkg.exec(cmd, function(error, stdout, stderr) {
