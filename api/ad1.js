c194ba45cc0bc78a500a5b7cb1a994432d3af0e9
b2blosangeles <b2blosangeles@gmail.com>
2018-02-12 10:41:11 -0800

    Delete ad1.js

--- api/ad1.js
+++
@@ -1,8 +0,0 @@
-
-pkg.fs.exists('/var/cert/', function(exists) {
-		let cmd_plus = (exists)?' && cd /var/cert/ && git pull ':'';
-		var cmd = 'cd ' + env.site_path + '&& git pull && cd ' + env.root_path + '&& git pull' + cmd_plus; 	
-		pkg.exec(cmd, function(error, stdout, stderr) {
-			res.send(stdout);
-		});
-	});
