e23b421eecbf676f80b9985aa91678adf0589e46
b2blosangeles <b2blosangeles@gmail.com>
2018-02-14 08:11:51 -0800

    Update test_stream.js

--- api/video/test_stream.js
+++ api/video/test_stream.js
@@ -1,3 +1,4 @@
+res.writeHead(200, {'Content-Type': 'image/jpeg'});
 pkg.request('https://cdn.lennar.net/images/com/images/new-homes/3/51/mhi/Huntington%20Beach%20Pier%20copy%20New.jpg?w=1200&h=540&as=1', 
 	function (error, response, body) {
 	res.send(body);
