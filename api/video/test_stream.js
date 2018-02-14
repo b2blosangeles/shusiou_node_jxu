1469cac5338c64f3bd4212031df90d8c6a99e6f1
b2blosangeles <b2blosangeles@gmail.com>
2018-02-14 11:51:12 -0800

    Update test_stream.js

--- api/video/test_stream.js
+++ api/video/test_stream.js
@@ -1,7 +1,7 @@
 var stream = require("stream")
 var a = new stream.PassThrough();
 a.pipe(res);
-let g = pkg.request('https://static.pexels.com/photos/248797/pexels-photo-248797.jpeg', 
+let g = pkg.request('http://198.199.120.18/api/video/test_niu.api?file=xaa', 
 	function (error, response, body) {
 });
 g.on('data', function(data) {
@@ -9,6 +9,15 @@ g.on('data', function(data) {
 	//a.write(new Buffer(data, 'binary'));
 });
 g.on('end', function(data) {
-	a.end()
+	let g1 = pkg.request('http://198.199.120.18/api/video/test_niu.api?file=xab', 
+		function (error, response, body) {
+	});
+	g1.on('data', function(data) {
+		a.write(data);
+		//a.write(new Buffer(data, 'binary'));
+	});
+	g1.on('end', function(data) {
+		a.end()
+	}
 });
 return true;
