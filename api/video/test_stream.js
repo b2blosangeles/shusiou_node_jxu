668cc39691f358ebf6c6798ebf95c4fb6b28c6d3
b2blosangeles <b2blosangeles@gmail.com>
2018-02-14 08:38:44 -0800

    Update test_stream.js

--- api/video/test_stream.js
+++ api/video/test_stream.js
@@ -1,7 +1,7 @@
 var stream = require("stream")
 var a = new stream.PassThrough();
-// res.set('Content-Type', 'image/png');
-res.set('Content-Type', 'text/plain');
+res.set('Content-Type', 'image/png');
+// res.set('Content-Type', 'text/plain');
 a.pipe(res);
 pkg.request('https://cdn.lennar.net/images/com/images/new-homes/3/51/mhi/Huntington%20Beach%20Pier%20copy%20New.jpg?w=1200&h=540&as=1', 
 	function (error, response, body) {
