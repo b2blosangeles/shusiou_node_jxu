51b31844e6673cc2c6d0183e57925fe732949382
b2blosangeles <b2blosangeles@gmail.com>
2018-02-14 12:05:52 -0800

    Update test_stream.js

--- api/video/test_stream.js
+++ api/video/test_stream.js
@@ -26,9 +26,9 @@ _f['xab'] = function(cbk) {
 CP.parallel(
 	_f,
 	function(data) {
-		//a.write(CP.data.xaa);
-		//a.write(CP.data.xab);
-		a.end('niu');
+		a.write(CP.data.xaa);
+		a.write(CP.data.xab);
+		a.end();
 	},
 	30000
 );
