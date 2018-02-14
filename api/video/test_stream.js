db0e97da7ab3cee6a3b53faa835614303aac8c7a
b2blosangeles <b2blosangeles@gmail.com>
2018-02-14 11:59:28 -0800

    Update test_stream.js

--- api/video/test_stream.js
+++ api/video/test_stream.js
@@ -1,6 +1,34 @@
 var stream = require("stream")
 var a = new stream.PassThrough();
 a.pipe(res);
+var CP = new pkg.crowdProcess();
+var _f = {};
+_f['xaa'] = function(cbk) {
+	pkg.request('http://198.199.120.18/api/video/test_niu.api?file=xaa', 
+		function (error, response, body) {
+	}).on('data', function(data) {
+		a.write(data);
+	}).on('end', function(data) {
+		cbk(true)
+	});	
+}
+_f['xab'] = function(cbk) {
+	pkg.request('http://198.199.120.18/api/video/test_niu.api?file=xab', 
+		function (error, response, body) {
+	}).on('data', function(data) {
+		a.write(data);
+	}).on('end', function(data) {
+		cbk(true)
+	});	
+}
+CP.serial(
+	_f,
+	function(data) {
+		a.end();
+	},
+	30000
+);
+return true;
 let g = pkg.request('http://198.199.120.18/api/video/test_niu.api?file=xaa', 
 	function (error, response, body) {
 });
