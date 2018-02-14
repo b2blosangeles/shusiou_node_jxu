88bde0f4b147d80c4e20b8193e07faabc1d642b8
b2blosangeles <b2blosangeles@gmail.com>
2018-02-14 11:36:53 -0800

    Update test_niu.js

--- api/video/test_niu.js
+++ api/video/test_niu.js
@@ -1,14 +1,1 @@
-var stream = require("stream")
-var a = new stream.PassThrough();
-a.pipe(res);
-let g = pkg.request('https://static.pexels.com/photos/248797/pexels-photo-248797.jpeg', 
-	function (error, response, body) {
-});
-g.on('data', function(data) {
-	a.write(data);
-	//a.write(new Buffer(data, 'binary'));
-});
-g.on('end', function(data) {
-	a.end()
-});
-return true;
+res.send('/var/img/xac');
