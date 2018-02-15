f2dfbb63992e41297b866e68b32e0614e5c1bd4b
b2blosangeles <b2blosangeles@gmail.com>
2018-02-15 15:04:52 -0800

    Update test_stream.js

--- api/video/test_stream.js
+++ api/video/test_stream.js
@@ -7,31 +7,33 @@ pkg.fs.readdir('/var/img/x/', (err, files) => {
 		// if (/x([a-z]+)/.test(file)) 
 		f[f.length] = file;
 	});
-	for (var i = 0; i < 17; i+=1) {
-	// for (var i = 0; i < f.length; i++) {
+	//for (var i = 0; i < 17; i+=1) {
+	for (var i = 0; i < f.length; i++) {
 		_f['P_' + i] = (function(i) { 
 			return function(cbk) {
 				let d = Buffer.from('');
 				pkg.request('https://shusiou1.nyc3.digitaloceanspaces.com/'+ f[i], 
 					function (error, response, body) {
 				}).on('data', function(data) {
-					d = Buffer.concat([d,  Buffer.from(data)]);
+					a.write(d);
+					// d = Buffer.concat([d,  Buffer.from(data)]);
 				}).on('end', function() {
-					cbk(d);
+					cbk(true);
 				});
 			}
 		})(i)
 	}
-	CP.parallel(
+	CP.serial(
+	//CP.parallel(
 		_f,
 		function(data) {
 			let stream = require("stream"),
 			    a = new stream.PassThrough();
 			
-			a.pipe(res);			
-			// for (var i = 0; i < f.length; i++) {
-			for (var i = 0; i < 17; i+=1) {
-				a.write(CP.data['P_' + i]);
+			// a.pipe(res);			
+			for (var i = 0; i < f.length; i++) {
+			//for (var i = 0; i < 17; i+=1) {
+			//	a.write(CP.data['P_' + i]);
 			}	
 			a.end();
 		},
