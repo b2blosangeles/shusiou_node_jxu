bfb813411b8e3628add6d5fa5314db00f28fd008
b2blosangeles <b2blosangeles@gmail.com>
2018-02-14 21:29:22 -0800

    Create test_aws.js

---
+++ api/video/test_aws.js
@@ -0,0 +1,26 @@
+res.send(env);
+return true;
+const AWS = require('/var/qalet/site/api/inc/aws-sdk/node_modules/aws-sdk')
+
+
+//Configure client for use with Spaces
+ const spacesEndpoint = new AWS.Endpoint('nyc3.digitaloceanspaces.com');
+ const s3 = new AWS.S3({
+     endpoint: spacesEndpoint,
+  accessKeyId: 'X7JSOMHQZTPIYDQ53VWH',
+                      secretAccessKey: 'stiDlHsoF5VA938FTkqk9iiRYzyEB1A6tjTJaLn+nIY'
+ });
+             // Add a file to a Space
+var params = {
+    Body: "uu The contents of the file",
+    Bucket: "shusiou1",
+    Key: "file.html",
+        ContentType: 'text/html',
+//      ContentLength: res.headers['content-length'],
+        ACL: 'public-read'
+};
+
+s3.putObject(params, function(err, data) {
+    if (err) console.log(err, err.message);
+    else     console.log(data);
+});
