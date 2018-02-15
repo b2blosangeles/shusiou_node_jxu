c117d72fb15ff0091ff23f478bb767c1b655860d
b2blosangeles <b2blosangeles@gmail.com>
2018-02-14 22:13:55 -0800

    Update test_aws.js

--- api/video/test_aws.js
+++ api/video/test_aws.js
@@ -1,29 +1,29 @@
 const AWS = require(env.site_path + '/api/inc/aws-sdk/node_modules/aws-sdk')
+const spacesEndpoint = new AWS.Endpoint('nyc3.digitaloceanspaces.com');
+const s3 = new AWS.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: 'X7JSOMHQZTPIYDQ53VWH',
+    secretAccessKey: 'stiDlHsoF5VA938FTkqk9iiRYzyEB1A6tjTJaLn+nIY'
+});
 
 pkg.fs.readFile('/var/img/xab', function (err, data0) {
   if (err) { throw err; }
+     var base64data = new Buffer(data0, 'binary');
+     //Configure client for use with Spaces
 
-   var base64data = new Buffer(data0, 'binary');
-   //Configure client for use with Spaces
-    const spacesEndpoint = new AWS.Endpoint('nyc3.digitaloceanspaces.com');
-    const s3 = new AWS.S3({
-        endpoint: spacesEndpoint,
-        accessKeyId: 'X7JSOMHQZTPIYDQ53VWH',
-        secretAccessKey: 'stiDlHsoF5VA938FTkqk9iiRYzyEB1A6tjTJaLn+nIY'
-    });
-                // Add a file to a Space
-   var params = {
-       Body: base64data,
-       Bucket: "shusiou1",
-       Key: "xab",
-           ContentType: 'image/jpg',
-   //      ContentLength: res.headers['content-length'],
-           ACL: 'public-read'
-   };
+                  // Add a file to a Space
+     var params = {
+         Body: base64data,
+         Bucket: "shusiou1",
+         Key: "xab",
+         ContentType: 'image/jpg',
+  //      ContentLength: res.headers['content-length'],
+         ACL: 'public-read'
+     };
 
-   s3.putObject(params, function(err, data) {
-       if (err) res.send(err.message);
-       else     res.send(data);
-   }); 
+     s3.putObject(params, function(err, data) {
+         if (err) res.send(err.message);
+         else     res.send(data);
+     }); 
 }); 
 
