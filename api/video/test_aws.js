c34a74990775af5852dd688c901509994c245934
b2blosangeles <b2blosangeles@gmail.com>
2018-02-16 13:40:42 -0800

    Update test_aws.js

--- api/video/test_aws.js
+++ api/video/test_aws.js
@@ -29,7 +29,7 @@ function removeFolder(s3, bucketName, folder, callback){
 	});
 }
 
-removeFolder(s3, 'shusiou1', 'niu/a/b/ab', function(data) {
+removeFolder(s3, 'shusiou1', 'niu/a/b/ac', function(data) {
 	res.send(data);
 });
 return true;
