4dc3a4d220d8ae0d6d32e3d9ac2dcce19df1d950
b2blosangeles <b2blosangeles@gmail.com>
2018-02-16 16:15:40 -0800

    Update test_aws.js

--- api/video/test_aws.js
+++ api/video/test_aws.js
@@ -5,20 +5,7 @@ const s3 = new AWS.S3({
     accessKeyId: 'AED442OVG2T3GE6IVPWQ',
     secretAccessKey: 'tvzSwhiJxlQ1RJNalUD0ATDeIZd0ko7P1Zs371J6Vi4'
 });
-let img_path = '/var/img/';
 
-let buff = new Buffer(100);
-pkg.fs.stat(img_path + 'video.mp4', function(err, stat) {
-	pkg.fs.open(img_path + 'video.mp4', 'r', function(err, fd) {
-		pkg.fs.read(fd, buff, 0, 100, 0, function(err, bytesRead, buffer) {
-			var start = buffer.indexOf(new Buffer('mvhd')) + 17;
-			var timeScale = buffer.readUInt32BE(start, 4);
-			var duration = buffer.readUInt32BE(start + 4, 4);
-			var movieLength = Math.floor(duration/timeScale);
-			res.send({fsize:stat.size,time_scale:timeScale, duration: duration, 'length:' + movieLength});
-		});
-	});
-});
 function removeFolder(s3, bucketName, folder, callback){
 	var params = {
 		Bucket: bucketName,
@@ -55,19 +42,22 @@ pkg.fs.readdir('/var/img/x/', (err, files) => {
 	});
 	var CP = new pkg.crowdProcess();
 	var _f = {}; 
-	_f['P_I'] = (function(i) { 
-		var buff = new Buffer(100);
-		pkg.fs.open(img_path + 'video.mp4', 'r', function(err, fd) {
-		  pkg.fs.read(fd, buff, 0, 100, 0, function(err, bytesRead, buffer) {
-		    var start = buffer.indexOf(new Buffer('mvhd')) + 17;
-		    var timeScale = buffer.readUInt32BE(start, 4);
-		    var duration = buffer.readUInt32BE(start + 4, 4);
-		    var movieLength = Math.floor(duration/timeScale);
-
-		    res.send('time scale: ' + timeScale + ' duration: ' + duration + ' movie length: ' + movieLength + ' seconds');
-		  });
+	_f['P_I'] = function(cbk) { 
+		let img_path = '/var/img/',
+		    buff = new Buffer(100);
+		pkg.fs.stat(img_path + 'video.mp4', function(err, stat) {
+			pkg.fs.open(img_path + 'video.mp4', 'r', function(err, fd) {
+				pkg.fs.read(fd, buff, 0, 100, 0, function(err, bytesRead, buffer) {
+					var start = buffer.indexOf(new Buffer('mvhd')) + 17;
+					var timeScale = buffer.readUInt32BE(start, 4);
+					var duration = buffer.readUInt32BE(start + 4, 4);
+					var movieLength = Math.floor(duration/timeScale);
+					cbk({fsize:stat.size,time_scale:timeScale, duration: duration, 'length:' + movieLength});
+				});
+			});
 		});
-	})(i)
+	};
+	
 	for (var i = 0; i < f.length; i++) {
 		_f['P_' + i] = (function(i) { 
 			return function(cbk) {
