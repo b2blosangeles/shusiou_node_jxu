190f95c39ee0f2a36db6889d9eb47f09e743beaf
b2blosangeles <b2blosangeles@gmail.com>
2018-02-16 22:44:54 -0800

    Update test_aws.js

--- api/video/test_aws.js
+++ api/video/test_aws.js
@@ -118,7 +118,7 @@ pkg.fs.readdir( tmp_folder, (err, files) => {
 						     s3.putObject(params, function(err, data) {
 							 if (err) cbk1(err.message);
 							 else {
-								 let v = CP.data['P_I1'];
+								 let v = JSON.parse(JSON.stringify(CP.data['P_I1']));
 								 v.x[v.x.length] = f[i];
 								 writeInfo(v, cbk1);
 								 // cbk1(data);
