98e79fb874441e48907a74064c2cb3ce25367997
b2blosangeles <b2blosangeles@gmail.com>
2018-02-18 08:11:30 -0800

    Update test_stream.js

--- api/video/test_stream.js
+++ api/video/test_stream.js
@@ -73,7 +73,7 @@ CP.serial(
 					pkg.request('https://shusiou01.nyc3.digitaloceanspaces.com/shusiou/movies1/' + fn[i], 
 					function (error, response, body) {})
 					.on('data', function(data) {
-						let s_data = (i === 0) ? Buffer.from(data.substring(0)) : Buffer.from(data);
+						let s_data = (i === 0) ? Buffer.from(data.substring(deltas)) : Buffer.from(data);
 						//let s_data = (i === 0) ? Buffer.from(data).readUInt32BE(deltas) : Buffer.from(data);
 						// let s_data = (i === 0) ? Buffer.from(data.substring(deltas)) : Buffer.from(data);
 						d = Buffer.concat([d,  s_data]);
