8f83b1b4ae3dafb14ea5c5892cc4cf804e69c013
b2blosangeles <b2blosangeles@gmail.com>
2018-02-18 06:19:33 -0800

    Update test_stream.js

--- api/video/test_stream.js
+++ api/video/test_stream.js
@@ -52,7 +52,7 @@ CP.serial(
 		}
 		var sidx = Math.floor(start/1048576);
 		fn = [cfg.x[sidx], cfg.x[sidx+1], cfg.x[sidx+3]];
-		res.send(cfg);
+		res.send(sidx);
 		return true;		
 		start = sidx * 1048576; end = (sidx + 1) * 1048576 * 3;
 	
