dc678e54e5eb92bd8b5e61b70137c8aff9899150
b2blosangeles <b2blosangeles@gmail.com>
2018-02-20 07:01:42 -0800

    Update join_stream.js

--- api/video/join_stream.js
+++ api/video/join_stream.js
@@ -1,10 +1,7 @@
-var mnt_folder = '/mnt/shusiou-video/';
-
 var config = require(env.config_path + '/config.json');
-
 var folderP = require(env.site_path + '/api/inc/folderP/folderP');
 
-var file = pkg.fs.createReadStream('/var/img/_x/video.aa');
+var file = pkg.fs.createReadStream('/var/img/_x/video.mp4/aa');
 file.pipe(res);
 file.on('close', function(){ 									
 });	
