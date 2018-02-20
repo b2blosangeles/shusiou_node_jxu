d1ae0df75e23687383b920c4e5dccbe2b91c0312
b2blosangeles <b2blosangeles@gmail.com>
2018-02-20 07:36:15 -0800

    Update join_stream.js

--- api/video/join_stream.js
+++ api/video/join_stream.js
@@ -1,32 +1,32 @@
-		let stream = require("stream"),
-		a = new stream.PassThrough();
-		a.pipe(res);
+let stream = require("stream"),
+a = new stream.PassThrough();
+a.pipe(res);
 var CP1 = new pkg.crowdProcess();
-		var _f1 = {}; 
-		var fn = ['aa', 'ab']
-		for (var i = 0; i < fn.length; i++) {
-			_f1['P_' + i] = (function(i) {
-				return function(cbk1) {
-					let d = Buffer.from('');
-					pkg.request('/var/img/_x/video.mp4/' + fn[i], 
-					function (error, response, body) {})
-					.on('data', function(data) {
-						d = Buffer.concat([d, Buffer.from(data)]);
-					}).on('end', function() {
-						cbk1(d);
-					});
-				}
-			})(i);	
+var _f1 = {}; 
+var fn = ['aa', 'ab']
+for (var i = 0; i < fn.length; i++) {
+	_f1['P_' + i] = (function(i) {
+		return function(cbk1) {
+			let d = Buffer.from('');
+			pkg.request('/var/img/_x/video.mp4/' + fn[i], 
+			function (error, response, body) {})
+			.on('data', function(data) {
+				d = Buffer.concat([d, Buffer.from(data)]);
+			}).on('end', function() {
+				cbk1(d);
+			});
 		}
+	})(i);	
+}
 
-		CP1.parallel(
-			_f1,
-			function(data) {
-				for (var i = 0; i < fn.length; i++) {
-					a.write(CP1.data['P_' + i]);
-				}	
-				a.end();
-			},
-			6000
-		);
+CP1.parallel(
+	_f1,
+	function(data) {
+		for (var i = 0; i < fn.length; i++) {
+			a.write(CP1.data['P_' + i]);
+		}	
+		a.end();
+	},
+	6000
+);
 
