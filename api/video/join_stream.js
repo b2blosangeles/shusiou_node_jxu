af2449febfdcb3eda2030b3bbba4f517e119ac5d
b2blosangeles <b2blosangeles@gmail.com>
2018-02-20 07:32:42 -0800

    Update join_stream.js

--- api/video/join_stream.js
+++ api/video/join_stream.js
@@ -1,8 +1,32 @@
-var config = require(env.config_path + '/config.json');
-var folderP = require(env.site_path + '/api/inc/folderP/folderP');
+		let stream = require("stream"),
+		a = new stream.PassThrough();
+		a.pipe(res);
+var CP1 = new pkg.crowdProcess();
+		var _f1 = {}; 
+		var fn = ['aa', 'ab']
+		for (var i = 0; i < fn.length; i++) {
+			_f1['P_' + i] = (function(i) {
+				return function(cbk1) {
+					let d = Buffer.from('');
+					pkg.request('/var/img/_x/video.mp4/' + fn[i], 
+					function (error, response, body) {})
+					.on('data', function(data) {
+						d = Buffer.concat([d, Buffer.from(data)]);
+					}).on('end', function() {
+						cbk1(d);
+					});
+				}
+			})(i);	
+		}
 
-var file = pkg.fs.createReadStream('/var/img/_x/video.mp4/aa');
-file.pipe(res);
-file.on('close', function(){ 									
-});	
+		CP1.parallel(
+			_f1,
+			function(data) {
+				for (var i = 0; i < fn.length; i++) {
+					a.write(CP1.data['P_' + i]);
+				}	
+				a.end();
+			},
+			6000
+		);
 
