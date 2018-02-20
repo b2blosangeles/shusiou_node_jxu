9e09da21f3af18bed725f327a59c631f8243f275
b2blosangeles <b2blosangeles@gmail.com>
2018-02-20 07:08:32 -0800

    Update join_stream.js

--- api/video/join_stream.js
+++ api/video/join_stream.js
@@ -1,7 +1,7 @@
 var config = require(env.config_path + '/config.json');
 var folderP = require(env.site_path + '/api/inc/folderP/folderP');
 
-var file = pkg.fs.createReadStream('/var/img/_x/video.mp4/ab');
+var file = pkg.fs.createReadStream('/var/img/_x/video.mp4/aa');
 file.pipe(res);
 file.on('close', function(){ 									
 });	
