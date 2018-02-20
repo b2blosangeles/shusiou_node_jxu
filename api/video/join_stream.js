8f290614f080dd2d45baff432f1dc6c38e6c5c4b
b2blosangeles <b2blosangeles@gmail.com>
2018-02-20 07:55:18 -0800

    Update join_stream.js

--- api/video/join_stream.js
+++ api/video/join_stream.js
@@ -11,7 +11,7 @@ a.pipe(res);
 var buff = new Buffer(100);
 var CP1 = new pkg.crowdProcess();
 var _f1 = {}; 
-var fn = ['aa', 'ab', 'ac', 'ad', 'ae'];
+var fn = ['aa', 'bb', 'bc', 'bd', 'be'];
 
 
 for (var i = 0; i < fn.length; i++) {
