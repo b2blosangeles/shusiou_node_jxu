8cb13998089256275b6bcdb21416930ccf5da9ba
b2blosangeles <b2blosangeles@gmail.com>
2018-02-19 21:05:54 -0800

    Update join_stream.js

--- api/video/join_stream.js
+++ api/video/join_stream.js
@@ -31,7 +31,7 @@ CP.serial(
 		var cfg = CP.data.P_I0;	
 		var CP1 = new pkg.crowdProcess();
 		var _f1 = {}; 
-	
+		/*
 		for (var i = 0; i < cfg.x.length; i++) {
 			_f1['P_' + i] = (function(i) {
 				return function(cbk1) {
@@ -56,8 +56,8 @@ CP.serial(
 			},
 			6000
 		);
-		
-		//res.send('---niu1---');
+		*/
+		res.send(cfg);
 	},
 	300000
 );
