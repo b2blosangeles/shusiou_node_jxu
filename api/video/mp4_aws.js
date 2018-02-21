f23aa6e1c23cd9e19f4bb3579c65300fa49ec4a8
b2blosangeles <b2blosangeles@gmail.com>
2018-02-21 07:25:01 -0800

    Update mp4_aws.js

--- api/video/mp4_aws.js
+++ api/video/mp4_aws.js
@@ -89,6 +89,8 @@ _f['INFO_1'] = function(cbk) {
 			a[a.length] = 'ffmpeg -i ' +  source_path + source_file + ' -t 00:00:10 -c copy ' +  tmp_folder + 'sec_' + a.length + 
 				'.mp4 -ss ' +  toHHMMSS(i);
 		}
+		cbk(a);
+		return true;
 		var CP1 = new pkg.crowdProcess();
 		var _f1 = {}
 		for (var i = 0 ; i < a.length; i+=10) {
