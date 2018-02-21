061464defcd6b22ce6095bc92877eb8d69ccb4dc
b2blosangeles <b2blosangeles@gmail.com>
2018-02-21 10:57:12 -0800

    Update mp4_aws.js

--- api/video/mp4_aws.js
+++ api/video/mp4_aws.js
@@ -63,6 +63,15 @@ var writeInfo = function(v, cbk) {
 	 else    cbk(v);
      });		
 }
+_f['CREATE_PATH_A'] = function(cbk) {
+	var folderP = require(env.site_path + '/api/inc/folderP/folderP');
+	var fp = new folderP();		
+	fp.build(tmp_folder, () => {	
+		cbk(true)
+	});
+	CP.exit = 1;
+};
+
 _f['INFO_0'] = function(cbk) { 
 	pkg.exec("ffprobe -i " + source_path + source_file + " -show_format -v quiet | sed -n 's/duration=//p'", 
 	function(error, stdout, stderr) {
@@ -74,13 +83,7 @@ _f['INFO_0'] = function(cbk) {
 		} else cbk(false);
 	});	
 };
-_f['P_A'] = function(cbk) {
-	var folderP = require(env.site_path + '/api/inc/folderP/folderP');
-	var fp = new folderP();		
-	fp.build(tmp_folder, () => {	
-		cbk(true)
-	});	
-};
+
 
 _f['INFO_1'] = function(cbk) { 
 	var videoLength = CP.data['INFO_0'], a = [];
