3b30ba82d1a151c30c3375cc1cc3faa55263ded3
b2blosangeles <b2blosangeles@gmail.com>
2018-02-22 08:18:58 -0800

    Update mp4_aws.js

--- api/video/mp4_aws.js
+++ api/video/mp4_aws.js
@@ -114,7 +114,15 @@ _f['PUSH_SECTION'] = function(cbk) {
 						pkg.exec('ffmpeg -i ' +  source_path + source_file + ' -t 00:00:10 -c copy ' +  
 							local_file +' -ss ' +  toHHMMSS(i) + ' -y', 
 							function(error, stdout, stderr) {
-//----
+										if (_info._x.indexOf(i) === -1) {
+											_info._x.push(i);
+											writeInfo(_info, cbk1);
+										} else {
+											cbk1(i + ' -- Done');
+										}
+							
+							//----
+							/*
 								pkg.fs.readFile(local_file, function (err, data0) {
 								  if (err) { throw err; }
 								     var base64data = new Buffer(data0, 'binary');
@@ -133,16 +141,13 @@ _f['PUSH_SECTION'] = function(cbk) {
 										 if (i === (f.length - 1)) {
 											v.status = 1;
 										 }
-//	----
-										if (_info._x.indexOf(i) === -1) {
-											_info._x.push(i);
-											writeInfo(_info, cbk1);
-										} else {
-											cbk1(i + ' -- Done');
-										}	 
+
+	 
 									 }	 
 								     });
 								});
+								*/
+							//	----
 							});
 					}	
 				}})(i);	
