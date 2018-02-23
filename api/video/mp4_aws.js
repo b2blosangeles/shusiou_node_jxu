eb33eaa7e44ac477c1b116bc548bd711c562a733
b2blosangeles <b2blosangeles@gmail.com>
2018-02-22 20:17:25 -0800

    Update mp4_aws.js

--- api/video/mp4_aws.js
+++ api/video/mp4_aws.js
@@ -113,11 +113,12 @@ _f['PUSH_SECTION'] = function(cbk) {
 						var local_fn = tmp_folder + 's_' + i + '_' + (i + 10) + '.mp4';
 						var space_fn =  space_dir + 's_' + i + '_' + (i + 10) + '.mp4';
 						pkg.exec('ffmpeg -i ' +  source_path + source_file + ' -vf trim=' + i + ':' + 
-							 (i + 10) + ' ' + local_fn +' -y', 
+							 (i + 10) + '' + local_fn +' -y', 
 							function(error, stdout, stderr) {		
 								pkg.fs.readFile(local_fn, function (err, data0) {
 								  if (err) { 
-									  cbk1(false);
+									  cbk1('ffmpeg -i ' +  source_path + source_file + ' -vf trim=' + i + ':' + 
+							 (i + 10) + ' ' + local_fn +' -y');
 									  return true
 									}
 								     var base64data = new Buffer(data0, 'binary');
