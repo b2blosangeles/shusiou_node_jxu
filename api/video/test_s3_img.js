c0cb9f9123a29ab3eb4f69c7e41a1d66f50c99f2
b2blosangeles <b2blosangeles@gmail.com>
2018-02-24 08:02:30 -0800

    Update test_s3_img.js

--- api/video/test_s3_img.js
+++ api/video/test_s3_img.js
@@ -30,7 +30,8 @@ _f['GET_X'] = function(cbk) {
 	});	
 };
 _f['GET_INFO'] = function(cbk) {
-
+	cbk('v');
+	return true;
 	let buff = new Buffer(100);
 	pkg.request(l[1], 
 		function (err, res, body) {
