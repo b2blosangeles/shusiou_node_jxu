11fe34648d7464b96275ebf7e83e27da7d98be3c
b2blosangeles <b2blosangeles@gmail.com>
2018-02-25 14:16:03 -0800

    Update test_s3_img.js

--- api/video/test_s3_img.js
+++ api/video/test_s3_img.js
@@ -148,7 +148,7 @@ CP.serial(
 			fn = dirn + '/cache.mp4';
 		}
 		pkg.fs.stat(fn, function(err, data1) {
-		      var total = parseInt(data1.size) + 1;
+		      var total = parseInt(data1.size);
 		      var range = req.headers.range;
 		      if (range) {
 				var parts = range.replace(/bytes=/, "").split("-");
