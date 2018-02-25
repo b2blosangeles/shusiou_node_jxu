dd8c2f81667ebe05bf197621946fd698af67be90
b2blosangeles <b2blosangeles@gmail.com>
2018-02-25 09:46:52 -0800

    Update test_s3_img.js

--- api/video/test_s3_img.js
+++ api/video/test_s3_img.js
@@ -10,10 +10,6 @@ let space_url = 'https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/_a';
 
 let 	info_link = '/video.mp4/_info.txt',
 	l = [
-		'/video.mp4/s_0.mp4',
-		'/video.mp4/s_10.mp4',
-		'/video.mp4/s_20.mp4',
-		'/video.mp4/s_30.mp4',
 		'/video.mp4/s_40.mp4',
 		'/video.mp4/s_50.mp4',
 		'/video.mp4/s_60.mp4',
