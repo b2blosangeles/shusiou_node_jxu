2069612cbf4955a2ceb52b47e35ef19602692ced
b2blosangeles <b2blosangeles@gmail.com>
2018-02-24 22:09:29 -0800

    Update test_s3_img.js

--- api/video/test_s3_img.js
+++ api/video/test_s3_img.js
@@ -39,22 +39,6 @@ function downloadFile(url, callback) {
 // a.pipe(res);
 // res.writeHead(200, {'Content-Type': 'video/mp4' });
 
-/*
-_f['GET_X'] = function(cbk) {
-	pkg.request(l[0], 
-	function (err, res, body) {
-		if (err) { 
-			cbk(false); 
-		} else {
-			let v = {};
-			try { 
-				v = JSON.parse(body);
-			} catch (e) { v = false; }
-			cbk(v);
-		}
-	});	
-};
-*/
 
 _f['GET_INF02'] = function(cbk) {
 	var CP1 = new pkg.crowdProcess();
@@ -82,40 +66,7 @@ _f['GET_INFO3'] = function(cbk) {
 	});
 };
 */
-/*
-_f['GET_FOLDERS'] = function(cbk) {
-	let buckets = CP.data.GET_BUCKETS.Buckets;
-	var CP1 = new pkg.crowdProcess(), _f1 = {};
-	for (let i = 0; i < buckets.length; i++) {
-		_f1[buckets[i].Name] = (function(i) {
-			return function(cbk1) {
-				var params = {
-					Bucket: buckets[i].Name //, 
-					// MaxKeys: 2
-				};
-				s3.listObjects(params, function(err, data) {
-					if (err) {
-						return cbk1(null); 
-					} else {
-						let l = data.Contents, v = [];
-						for (var  j = 0 ; j < l.length; j++) {
-							v.push({link: space_url + '' + l[j].Key, size:l[j].Size});
-						}
-						return cbk1(v);       
-					}
-				});
-			}
-		})(i);
-	}
-	CP1.serial(
-		_f1,
-		function(results) {
-			cbk(results.results);
-		},
-		60000	
-	)
-};
-*/
+
 CP.serial(
 	_f,
 	function(results) {
