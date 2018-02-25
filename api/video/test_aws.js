f6152fd89ac26ebbd783a097142b0c0316f48508
b2blosangeles <b2blosangeles@gmail.com>
2018-02-25 15:29:11 -0800

    Update test_aws.js

--- api/video/test_aws.js
+++ api/video/test_aws.js
@@ -62,7 +62,7 @@ _f['P_0'] = function(cbk) {
 				var timeScale = buffer.readUInt32BE(start, 4);
 				var duration = buffer.readUInt32BE(start + 4, 4);
 				var movieLength = Math.floor(duration/timeScale);
-				var v = {filesize:stat.size,time_scale:timeScale, trunksize: trunkSize,
+				var v = {filesize:stat.size, start:start, time_scale:timeScale, trunksize: trunkSize,
 					duration: duration, length:movieLength, x:[], status:0};
 				cbk(v);
 			});
