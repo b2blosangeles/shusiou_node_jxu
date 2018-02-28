1704a967d02d88bf5c9f9efbc57c63d69d9848f3
b2blosangeles <b2blosangeles@gmail.com>
2018-02-28 15:40:27 -0800

    Update test1.js

--- api/video/test1.js
+++ api/video/test1.js
@@ -7,12 +7,12 @@ a.pipe(res);
 res.writeHead(200, {'Content-Type': 'video/mp4' });
 
 var d = Buffer.from('');
-pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/jxu.mp4', 
+pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/aa', 
 function (error, response, body) {})
 .on('data', function(data) {
 	d = Buffer.concat([d, Buffer.from(data)]);
 }).on('end', function() {
-	pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/jxu.mp4', 
+	pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/ab', 
 	function (error1, response1, body1) {}).
 	on('data', function(data1) {
 		d = Buffer.concat([d, Buffer.from(data1)]);
