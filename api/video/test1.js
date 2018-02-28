9a2549e307b1b3d2d3e8a73a7b01894d7f54c9b7
b2blosangeles <b2blosangeles@gmail.com>
2018-02-28 15:38:37 -0800

    Update test1.js

--- api/video/test1.js
+++ api/video/test1.js
@@ -12,7 +12,7 @@ function (error, response, body) {})
 .on('data', function(data) {
 	d = Buffer.concat([d, Buffer.from(data)]);
 }).on('end', function() {
-	pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/ab', 
+	pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/jxu.mp4', 
 	function (error1, response1, body1) {}).
 	on('data', function(data1) {
 		d = Buffer.concat([d, Buffer.from(data1)]);
