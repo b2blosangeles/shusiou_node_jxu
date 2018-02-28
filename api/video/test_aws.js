f855681d667b341f22f3dfc6f2894b7c4a1385b7
b2blosangeles <b2blosangeles@gmail.com>
2018-02-28 01:53:39 -0800

    Update test_aws.js

--- api/video/test_aws.js
+++ api/video/test_aws.js
@@ -79,7 +79,7 @@ _f['CREATE_DIR'] = function(cbk) {
 				let ts_file =  source_file.replace(/\.mp4$/, '.ts');
 				pkg.exec('cd ' + source_path + ' && ffmpeg -i ' + source_file +
 					 ' -c copy -bsf:v h264_mp4toannexb -f mpegts ' +  ts_file +
-					 ' -y && split -b ' + trunkSize + ' ' +  ts_file +  ' ' + tmp_folder + ' -y', 
+					 ' -y && split -b ' + trunkSize + ' ' +  ts_file +  ' ' + tmp_folder + '', 
 				function(error, stdout, stderr) {
 					if (error) cbk(false);
 					else if (stdout) cbk(true);
