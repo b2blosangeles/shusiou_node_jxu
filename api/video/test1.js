e52663c2b030586a2b0a1129e2a49f7a1bb18b74
b2blosangeles <b2blosangeles@gmail.com>
2018-02-28 17:00:47 -0800

    Update test1.js

--- api/video/test1.js
+++ api/video/test1.js
@@ -14,6 +14,7 @@ function (error, response, body) {})
 .on('data', function(data) {
 	d = Buffer.concat([d, Buffer.from(data)]);
 }).on('end', function() {
+	/*
 	pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/dm', 
 	function (error1, response1, body1) {}).
 	on('data', function(data1) {
@@ -22,7 +23,8 @@ function (error, response, body) {})
 		a.write(d);
 		a.end();
 	});
-	//a.write(d);
-	//a.end();
+	*/
+	a.write(d);
+	a.end();
 });
 return true;
