5df5951afd65633ee7da4004701fdf5a2915d37a
b2blosangeles <b2blosangeles@gmail.com>
2018-03-01 14:32:57 -0800

    Update test1.js

--- api/video/test1.js
+++ api/video/test1.js
@@ -10,18 +10,18 @@ res.writeHead(206, {'Content-Range': 'bytes ' + 0 + '-' + (total-1) + '/' + tota
 // res.writeHead(200, { 'Content-Type': 'video/mp4' });
 
 var d = Buffer.from('');
-pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/aa', 
+pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/output000.mp4', 
 function (error, response, body) {})
 .on('data', function(data) {
 	d = Buffer.concat([d, Buffer.from(data)]);
 }).on('end', function() {
 	
-	pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/ab', 
+	pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/output001.mp4', 
 	function (error1, response1, body1) {}).
 	on('data', function(data1) {
 		d = Buffer.concat([d, Buffer.from(data1)]);
 	}).on('end', function() {	
-		pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/ac', 
+		pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/output002.mp4', 
 		function (error2, response2, body2) {}).
 		on('data', function(data2) {
 			d = Buffer.concat([d, Buffer.from(data2)]);
