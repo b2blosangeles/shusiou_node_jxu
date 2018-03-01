f608ab547e676c26f6bcb35118e2108ce7600131
b2blosangeles <b2blosangeles@gmail.com>
2018-02-28 16:43:08 -0800

    Update test1.js

--- api/video/test1.js
+++ api/video/test1.js
@@ -9,18 +9,21 @@ res.writeHead(200, {'Content-Range': 'bytes ' + 0 + '-' + total + '/' + total,
     'Accept-Ranges': 'bytes', 'Content-Type': 'video/mp4' });
 
 var d = Buffer.from('');
-pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/jxu.mp4', 
+pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/aa', 
 function (error, response, body) {})
 .on('data', function(data) {
 	d = Buffer.concat([d, Buffer.from(data)]);
 }).on('end', function() {
-	pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/jxu.mp4', 
+	/*
+	pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/ab', 
 	function (error1, response1, body1) {}).
 	on('data', function(data1) {
 		d = Buffer.concat([d, Buffer.from(data1)]);
 	}).on('end', function() {	
 		a.write(d);
 		a.end();
-	});	
+	});*/
+	a.write(d);
+	a.end();
 });
 return true;
