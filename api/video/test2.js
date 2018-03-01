1bd29484094ee37bd125ec7916adb4a99d11c501
b2blosangeles <b2blosangeles@gmail.com>
2018-03-01 15:47:05 -0800

    Update test2.js

--- api/video/test2.js
+++ api/video/test2.js
@@ -1,5 +1,6 @@
 var total = 1024*1024;
+/*
 res.writeHead(206, {'Content-Range': 'bytes ' + 0 + '-' + (total-1) + '/' + total, 
-    'Accept-Ranges': 'bytes', 'Content-Type': 'video/mp4' });
+    'Accept-Ranges': 'bytes', 'Content-Type': 'video/mp4' });*/
 var file = pkg.fs.createReadStream('/var/img/video.mp4', {start:0, end:1024*1024});
 file.pipe(res);
