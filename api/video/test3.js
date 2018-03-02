f85b0146a782234e915b94d4a657849aa5d09e7f
b2blosangeles <b2blosangeles@gmail.com>
2018-03-01 16:18:24 -0800

    Create test3.js

---
+++ api/video/test3.js
@@ -0,0 +1,9 @@
+var total = 1024*1024;
+/*
+if ((req.headers) && (req.headers.range)) {
+        res.writeHead(206, {'Content-Range': 'bytes ' + 0 + '-' + (total-1) + '/' + total, 
+         'Accept-Ranges': 'bytes', 'Content-Type': 'video/mp4' });
+} 
+*/
+var file = pkg.fs.createReadStream('/var/img/video.mp4', {start:0, end:1024*1024});
+file.pipe(res);
