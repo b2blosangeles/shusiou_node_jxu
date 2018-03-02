6a0debcf4227e28ec320ae88831782e0b9e7b350
b2blosangeles <b2blosangeles@gmail.com>
2018-03-01 17:04:35 -0800

    Update test3.js

--- api/video/test3.js
+++ api/video/test3.js
@@ -6,9 +6,10 @@ var d = Buffer.from('');
 pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/output000.mp4', 
 function (error, response, body) {})
 .on('data', function(data) {
-	d = Buffer.concat([d, Buffer.from(data)]);
+	// d = Buffer.concat([d, Buffer.from(data)]);
+	a.write(data);
 }).on('end', function() {
-	 a.write(d);
+	// a.write(d);
 	 a.end();
 });
 //var file = pkg.fs.createReadStream('/var/img/video.mp4', {start:0, end:1024*1024});
