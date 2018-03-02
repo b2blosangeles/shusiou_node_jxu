daff83f3cda50816407ab20b71bd946767afbb4f
b2blosangeles <b2blosangeles@gmail.com>
2018-03-01 22:01:32 -0800

    Update test3.js

--- api/video/test3.js
+++ api/video/test3.js
@@ -1,28 +1,26 @@
-let stream = require("stream"),
-a = new stream.PassThrough();
-a.pipe(res);
-
-var total = 1024*1024;
-var d = Buffer.from('');
-
 var CP = new pkg.crowdProcess();
 var _f = {}, fn = ['output000.mp4', 'output001.mp4']; 
 
 for (var i = 0; i < fn.length; i++) {
 	_f['P_' + i] = (function(i) {
 		return function(cbk) {
-			let d = Buffer.from('');
-			pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/' + fn[i], 
-			function (error, response, body) {})
-			.on('data', function(data) {
-				d = Buffer.concat([d, Buffer.from(data)]);
-			}).on('end', function() {
-				cbk1(d);
+			let url = 'https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/' + fn[i];
+			let file = pkg.fs.createWriteStream('var/img/M_' + i + '.mp4');
+			file.on('finish', function() {
+				file.close(function() {
+					cbk(fn[i]);
+				});  
 			});
+			pkg.request(url, function (error, response, body) {
+			}).pipe(file);			      
 		}
 	})(i);	
 }
-
+CP.parallel(
+	_f,
+	function(results) {
+		cbk(results);
+	}, 8000);
 /*
 pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/output001.mp4', 
 function (error, response, body) {})
@@ -38,7 +36,7 @@ function (error, response, body) {})
 //    req.pipe(x)
  //   x.pipe(res)
 
-req.pipe(pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/output001.mp4')).pipe(res);
+// req.pipe(pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/output001.mp4')).pipe(res);
 
 /*
 pkg.request
