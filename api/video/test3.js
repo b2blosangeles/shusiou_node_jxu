c92f102d85edc8a5c6243fabbd8ace9f9faa739a
b2blosangeles <b2blosangeles@gmail.com>
2018-03-01 22:50:40 -0800

    Update test3.js

--- api/video/test3.js
+++ api/video/test3.js
@@ -21,7 +21,11 @@ _f['PULLING'] = function(cbk) {
 				let file = pkg.fs.createWriteStream('/var/img/M_' + i + '.mp4');
 				file.on('finish', function() {
 					file.close(function() {
-						cbk1(fn[i]);
+						let cmd = 'cd ' + dirn + ' && ffmpeg -i M_' + i + '.mp4  -c copy -bsf:v h264_mp4toannexb -f mpegts  M_' + i + '.ts -y';
+						pkg.exec(cmd, 
+							function(error, stdout, stderr) {
+								cbk1(fn[i]);;
+						});
 					});  
 				});
 				pkg.request(url, function (error, response, body) {
