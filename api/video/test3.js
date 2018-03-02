2cacf64d31af0e41c3f89f37dc0d5dfc0b031b3f
b2blosangeles <b2blosangeles@gmail.com>
2018-03-01 22:26:22 -0800

    Update test3.js

--- api/video/test3.js
+++ api/video/test3.js
@@ -8,35 +8,37 @@ _f['WRITE_TXT'] = function(cbk) {
 		str += "file '" + 'M_' + i + ".mp4'\n";
 	}
 	pkg.fs.writeFile(dirn + 'engine.data', str, function(err) {	    
-		cbk('niu');
+		cbk('WRITE_TXT:' + dirn + 'engine.data');
 	}); 
 };
+_f['PULLING'] = function(cbk) {
+	var CP1 = new pkg.crowdProcess();
+	var _f1 = {}; 
+
+	for (var i = 0; i < fn.length; i++) {
+		_f1['P_' + i] = (function(i) {
+			return function(cbk1) {
+				let url = 'https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/' + fn[i];
+				let file = pkg.fs.createWriteStream('/var/img/M_' + i + '.mp4');
+				file.on('finish', function() {
+					file.close(function() {
+						cbk1(fn[i]);
+					});  
+				});
+				pkg.request(url, function (error, response, body) {
+				}).pipe(file);	
+			}
+		})(i);	
+	}
+	CP1.parallel(
+	_f1,
+	function(results) {
+		cbk.send(results);
+	}, 6000);
+}
 CP.serial(_f,
 	function(results) {
 		res.send(results);
 	}, 8000);
 return true;
 
-var CP1 = new pkg.crowdProcess();
-var _f1 = {}; 
-
-for (var i = 0; i < fn.length; i++) {
-	_f1['P_' + i] = (function(i) {
-		return function(cbk) {
-			let url = 'https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/' + fn[i];
-			let file = pkg.fs.createWriteStream('/var/img/M_' + i + '.mp4');
-			file.on('finish', function() {
-				file.close(function() {
-					cbk(fn[i]);
-				});  
-			});
-			pkg.request(url, function (error, response, body) {
-			}).pipe(file);	
-		}
-	})(i);	
-}
-CP1.parallel(
-_f1,
-function(results) {
-	res.send(results);
-}, 8000);
