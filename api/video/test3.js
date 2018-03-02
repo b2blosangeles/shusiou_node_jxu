d295d068562b57ca4295ad1751c1708a132079be
b2blosangeles <b2blosangeles@gmail.com>
2018-03-01 17:18:45 -0800

    Update test3.js

--- api/video/test3.js
+++ api/video/test3.js
@@ -1,19 +1,20 @@
 //let stream = require("stream"),
 //a = new stream.PassThrough();
 //a.pipe(res);
-/*
+
 var total = 1024*1024;
 //var d = Buffer.from('');
 pkg.request('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/output000.mp4', 
 function (error, response, body) {})
 .on('data', function(data) {
 	// d = Buffer.concat([d, Buffer.from(data)]);
-//	a.write(data);
+	a.write(Buffer.from(data)]);
 }).on('end', function() {
 	// a.write(d);
-//	 a.end();
+	 a.end();
 }).pipe(res);
-*/
+
+/*
 pkg.request
   .get('https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/v/output000.mp4')
   .on('response', function(response) {
@@ -22,6 +23,6 @@ pkg.request
   })
   .pipe(res);
 
-
+*/
 //var file = pkg.fs.createReadStream('/var/img/video.mp4', {start:0, end:1024*1024});
 //file.pipe(res);
