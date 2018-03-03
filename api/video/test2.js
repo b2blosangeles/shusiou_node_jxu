d4a1ecdf2adbd2ffa13fdfccf81d739a82a9dac1
b2blosangeles <b2blosangeles@gmail.com>
2018-03-03 06:57:24 -0800

    Update test2.js

--- api/video/test2.js
+++ api/video/test2.js
@@ -5,6 +5,6 @@ if ((req.headers) && (req.headers.range)) {
          'Accept-Ranges': 'bytes', 'Content-Type': 'video/mp4' });
 } 
 */
-//var file = pkg.fs.createReadStream('/var/img/cache.mp4');
-//file.pipe(res);
-res.sendFile('/var/img/_s/video.mp4/s_80.mp4');
+var file = pkg.fs.createReadStream('/var/img/_s/video.mp4/s_80.mp4');
+file.pipe(res);
+// res.sendFile('/var/img/_s/video.mp4/s_80.mp4');
