53f864b5cd7b3705db4a421815b5b39c5e3afcf1
b2blosangeles <b2blosangeles@gmail.com>
2018-03-03 01:06:15 -0800

    Update combine_play.js

--- api/video/combine_play.js
+++ api/video/combine_play.js
@@ -56,12 +56,12 @@ _f['FFMPEG'] = function(cbk) {
 CP.serial(_f,
 	function(results) {
   //      res.writeHead(206, { 'Content-Type': 'video/mp4' });	
-		var file = pkg.fs.createReadStream('/var/img/cache.mp4');
-		file.pipe(res);
+		//var file = pkg.fs.createReadStream('/var/img/cache.mp4');
+		//file.pipe(res);
 	//	var file = pkg.fs.createReadStream('/var/img/video.mp4');
 	//	file.pipe(res);	
 	
-		//res.sendFile('/var/img/cache.mp4');
+		res.sendFile('/var/img/cache.mp4');
 	}, 8000);
 return true;
 
