33352cbc8e5bae624a107e22cb537d9edec3d072
b2blosangeles <b2blosangeles@gmail.com>
2018-03-04 05:26:03 -0800

    Create pipe.js

---
+++ api/video/pipe.js
@@ -0,0 +1,73 @@
+let CP = new pkg.crowdProcess();
+let _f = {}, fn = ['s_0.mp4', 's_1.mp4', 's_2.mp4', 's_3.mp4']; 
+let dirn = '/var/img/';
+
+_f['WRITE_TXT'] = function(cbk) {
+	var str = '';
+	for (var i = 0; i < fn.length; i++) {
+		str += "file '" + dirn + 'M_' + i + ".mp4'\n";
+	}
+	pkg.fs.writeFile(dirn + 'engine.data', str, function(err) {	    
+		cbk('WRITE_TXT:' + dirn + 'engine.data');
+	}); 
+};
+_f['PULLING'] = function(cbk) {;
+	var CP1 = new pkg.crowdProcess();
+	var _f1 = {}; 
+	for (var i = 0; i < fn.length; i++) {
+		_f1['P_' + i] = (function(i) {
+			return function(cbk1) {
+				let url = 'https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/video.mp4/_s/' + fn[i];
+				let file = pkg.fs.createWriteStream('/var/img/M_' + i + '.mp4');
+				file.on('finish', function() {
+					file.close(function() {
+						cbk1(fn[i]);
+						return true;
+						let cmd = 'cd ' + dirn + ' && ffmpeg -i M_' + i + '.mp4 ' +
+						    ' -c copy -bsf:v h264_mp4toannexb -f mpegts  M_' + i + '.ts -y ' +
+						    ' && rm M_' + i + '.mp4';
+						pkg.exec(cmd, 
+							function(error, stdout, stderr) {
+								cbk1(fn[i]);
+						});
+					});  
+				});
+				pkg.request(url, function (error, response, body) {
+				}).pipe(file);	
+			}
+		})(i);	
+	}
+	CP1.parallel(
+	_f1,
+	function(results) {
+		cbk(results);
+	}, 6000);
+}
+
+_f['FFMPEG'] = function(cbk) {
+	// let cmd = 'cd ' + dirn + ' && ffmpeg -f concat -safe 0 -i ' + dirn + 'engine.data -codec copy cache.mp4 -y';
+	let cmd = 'cd ' + dirn + ' && ffmpeg -f concat -safe 0 -i ' + dirn + 'engine.data -c copy cache.mp4 -y';
+	pkg.exec(cmd, 
+		function(error, stdout, stderr) {
+			cbk(cmd);	
+	});
+};
+_f['FFMPEG_SECTION'] = function(cbk) {
+	let cmd = 'cd ' + dirn + ' && ffmpeg -ss 00:00:01 -i cache.mp4 -ss 00:00:01 -t 00:00:07  -c copy -y out121.mp4';
+	pkg.exec(cmd, 
+		function(error, stdout, stderr) {
+			cbk(cmd);	
+	});
+};
+CP.serial(_f,
+	function(results) {
+        	// res.writeHead(206, { 'Content-Type': 'video/mp4' });	
+		var file = pkg.fs.createReadStream('/var/img/out121.mp4');
+		file.pipe(res);
+	//	var file = pkg.fs.createReadStream('/var/img/video.mp4');
+	//	file.pipe(res);	
+	
+	//	res.sendFile('/var/img/cache.mp4');
+	}, 8000);
+return true;
+
