18c12bff70fe65d9f5a990205f35dff0d0162e4c
b2blosangeles <b2blosangeles@gmail.com>
2018-03-04 05:39:27 -0800

    Update pipe.js

--- api/video/pipe.js
+++ api/video/pipe.js
@@ -1,18 +1,18 @@
 let space = {
 	endpoint : 'https://shusiou-d-01.nyc3.digitaloceanspaces.com/shusiou/',
-	video:'video.mp4'
+	video:'video.mp4',
+	cache_folder: '/var/img/'
 }
 let CP = new pkg.crowdProcess();
 let _f = {}, fn = ['s_0.mp4', 's_1.mp4', 's_2.mp4', 's_3.mp4']; 
-let dirn = '/var/img/';
 
 _f['WRITE_TXT'] = function(cbk) {
 	var str = '';
 	for (var i = 0; i < fn.length; i++) {
-		str += "file '" + dirn + 'M_' + i + ".mp4'\n";
+		str += "file '" + space.cache_folder + 'M_' + i + ".mp4'\n";
 	}
-	pkg.fs.writeFile(dirn + 'engine.data', str, function(err) {	    
-		cbk('WRITE_TXT:' + dirn + 'engine.data');
+	pkg.fs.writeFile(space.cache_folder  + 'engine.data', str, function(err) {	    
+		cbk('WRITE_TXT:' + space.cache_folder  + 'engine.data');
 	}); 
 };
 _f['PULLING'] = function(cbk) {;
@@ -26,14 +26,6 @@ _f['PULLING'] = function(cbk) {;
 				file.on('finish', function() {
 					file.close(function() {
 						cbk1(fn[i]);
-						return true;
-						let cmd = 'cd ' + dirn + ' && ffmpeg -i M_' + i + '.mp4 ' +
-						    ' -c copy -bsf:v h264_mp4toannexb -f mpegts  M_' + i + '.ts -y ' +
-						    ' && rm M_' + i + '.mp4';
-						pkg.exec(cmd, 
-							function(error, stdout, stderr) {
-								cbk1(fn[i]);
-						});
 					});  
 				});
 				pkg.request(url, function (error, response, body) {
@@ -49,22 +41,21 @@ _f['PULLING'] = function(cbk) {;
 }
 
 _f['FFMPEG'] = function(cbk) {
-	// let cmd = 'cd ' + dirn + ' && ffmpeg -f concat -safe 0 -i ' + dirn + 'engine.data -codec copy cache.mp4 -y';
-	let cmd = 'cd ' + dirn + ' && ffmpeg -f concat -safe 0 -i ' + dirn + 'engine.data -c copy cache.mp4 -y';
+	let cmd = 'cd ' + space.cache_folder  + ' && ffmpeg -f concat -safe 0 -i ' + space.cache_folder  + 'engine.data -c copy cache.mp4 -y';
 	pkg.exec(cmd, 
 		function(error, stdout, stderr) {
 			cbk(cmd);	
 	});
 };
 _f['FFMPEG_SECTION'] = function(cbk) {
-	let cmd = 'cd ' + dirn + ' && ffmpeg -ss 00:00:01 -i cache.mp4 -ss 00:00:01 -t 00:00:07  -c copy -y out121.mp4';
+	let cmd = 'cd ' +space.cache_folder  + ' && ffmpeg -ss 00:00:01 -i cache.mp4 -ss 00:00:01 -t 00:00:07  -c copy -y out121.mp4';
 	pkg.exec(cmd, 
 		function(error, stdout, stderr) {
 			cbk(cmd);	
 	});
 };
 _f['FFMPEG_IMG'] = function(cbk) {
-	let cmd =  'cd ' + dirn + ' && ffmpeg -i out121.mp4 -ss 00:00:02 -vframes 1 -preset ultrafast out2.png -y';
+	let cmd =  'cd ' + space.cache_folder  + ' && ffmpeg -i out121.mp4 -ss 00:00:02 -vframes 1 -preset ultrafast out2.png -y';
 	pkg.exec(cmd, 
 		function(error, stdout, stderr) {
 			cbk(cmd);	
