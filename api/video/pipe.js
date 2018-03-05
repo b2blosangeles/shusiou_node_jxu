92cf940f0bff18cda6632c4c6c6eb28a7c9febbc
b2blosangeles <b2blosangeles@gmail.com>
2018-03-05 15:56:20 -0800

    Update pipe.js

--- api/video/pipe.js
+++ api/video/pipe.js
@@ -40,7 +40,7 @@ var ss0 = req.query['ss'],
     ss = Math.floor(ss0),
     d_s = ss0 - ss,
     sec_s = Math.floor(parseInt(ss) / 5), 
-    t = (isNaN(req.query['t'])) ? 0 : parseFloat(req.query['t']),
+    t = (isNaN(req.query['t'])) ? 0 : req.query['t'],
     sec_t =  Math.ceil(parseInt(t) / 5), 
     start_point = parseInt(ss) % 5 + d_s;
 
