ed62ef5ba19ac296738820a2c26f15b1f789123b
b2blosangeles <b2blosangeles@gmail.com>
2018-03-05 15:54:28 -0800

    Update pipe.js

--- api/video/pipe.js
+++ api/video/pipe.js
@@ -114,8 +114,7 @@ _f['FFMPEG_SECTION'] = function(cbk) {
 	if (!sec_t) {
 		cbk(false);
 	} else {
-		var cmd = 'cd ' + space.cache_folder + ' && ffmpeg -ss ' + sec_s + ' -i cache_' + sec_s + '_' + sec_t + '.mp4 ' +
-		    ' -t ' + t + '  -c copy -y tmpcache_' + s + '_' + t + '.mp4';
+		var cmd = 'cd ' + space.cache_folder + ' && ffmpeg -ss ' + sec_s + ' -i cache_' + sec_s + '_' + sec_t + '.mp4  -t ' + t + ' -c copy -y tmpcache_' + s + '_' + t + '.mp4';
 		cbk(cmd);
 		/*
 		pkg.exec(cmd, 
