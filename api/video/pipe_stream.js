fefaf4ce16150abdb8676cfae5ed0b2e15805bcb
b2blosangeles <b2blosangeles@gmail.com>
2018-03-06 14:58:02 -0800

    Update pipe_stream.js

--- api/video/pipe_stream.js
+++ api/video/pipe_stream.js
@@ -74,7 +74,7 @@ _f['FFMPEG_SECTION'] = function(cbk) {
 CP.serial(_f,
 	function(results) {
 		let totalsize = CP.data.FFMPEG_SECTION.filesize;
-		res.send(totalsize);
+		res.send(totalsize.toString());
 		return true;
 		let url =  space.cache_folder + 's_0.mp4';
 		pkg.fs.stat( url, function(err, stat) {
