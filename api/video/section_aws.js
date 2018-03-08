44db6067b884ad1f699eca78e0400d709a5e746c
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 07:23:43 -0800

    Update section_aws.js

--- api/video/section_aws.js
+++ api/video/section_aws.js
@@ -93,7 +93,10 @@ function trackAws(_file, _cbk)  {
 							var duration = buffer.readUInt32BE(start + 4, 4);
 							var movieLength = Math.floor(duration/timeScale);
 							var v = {filesize:stat.size,time_scale:timeScale, trunksize: trunkSize,
-								duration: duration, length:movieLength, x:[], status:0};
+								duration: duration, length:movieLength, status:0};
+							v['_s'] = []; 
+							if (!v['status']) v['status'] = {}; 
+							v['status']['_s'] = 0;
 							writeInfo(v, function() {
 								cbk(v);
 							});
