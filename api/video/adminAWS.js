88e0c1932b7f38229739ec8361102b73a96a1bde
b2blosangeles <b2blosangeles@gmail.com>
2018-03-09 12:14:47 -0800

    Update adminAWS.js

--- api/video/adminAWS.js
+++ api/video/adminAWS.js
@@ -25,7 +25,7 @@ let folder = '';
 browseFolder(folder , function(data) {
 	let list = [];
 	for (var i = 0; i < data.Contents.length; i++) {
-		list.push(data..Contents[i].Key);
+		list.push(data.Contents[i].Key);
 	}
 	res.send({tm:new Date().getTime() - tm, list});
 });
