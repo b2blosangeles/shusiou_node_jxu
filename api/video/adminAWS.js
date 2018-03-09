9b5342f2e3bd27c6ff40ccbbad47330f0a897ef6
b2blosangeles <b2blosangeles@gmail.com>
2018-03-09 10:05:41 -0800

    Create adminAWS.js

---
+++ api/video/adminAWS.js
@@ -0,0 +1,29 @@
+	const AWS = require(env.site_path + '/api/inc/aws-sdk/node_modules/aws-sdk')
+	const spacesEndpoint = new AWS.Endpoint('nyc3.digitaloceanspaces.com');
+	const s3 = new AWS.S3({
+	    endpoint: spacesEndpoint,
+	    accessKeyId: 'QYF3ENCI4TEDFDWFBS6N',
+	    secretAccessKey: '7DJD8b9iAqD5qsLgRZH9OXfgOQMob/edWouwiqYeOwI'
+	});
+
+	let tm = new Date().getTime();
+
+	function browseFolder(folder, cbk){
+		var params = {
+			Bucket: 'shusiou-d-01',
+			Prefix: folder
+		};
+		s3.listObjects(params, function(err, data) {
+			   cbk(data)
+
+		});
+	}
+
+let folder = '/';
+  
+  browseFolder(folder, function(data) {
+	  
+    res.send({tm:new Date().getTime() - tm,data:data});
+  });
+  
+  
