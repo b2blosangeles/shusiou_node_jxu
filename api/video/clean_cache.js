ba85b086932601549e9b3896a8b4d9b4a6a47929
b2blosangeles <b2blosangeles@gmail.com>
2018-03-14 18:47:39 -0700

    Update clean_cache.js

--- api/video/clean_cache.js
+++ api/video/clean_cache.js
@@ -2,13 +2,10 @@ var finder = require(env.site_path + '/api/inc/findit/findit.js')('/tmp/shusiou_
 var path = require('path');
 let list = [];
 finder.on('directory', function (dir, stat, stop) {
- //   var base = path.basename(dir);
- //   if (base === '.git' || base === 'node_modules') stop()
- //   else console.log(dir + '/')
 });
 
 finder.on('file', function (file, stat) {
-     list.push(file);
+     list.push({fn:file, created:state.ctime});
 });
 
 finder.on('link', function (link, stat) {
