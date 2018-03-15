c5600a0c1ff028453c85048fca62728a82fed529
b2blosangeles <b2blosangeles@gmail.com>
2018-03-15 06:50:39 -0700

    Update clean_cache.js

--- api/video/clean_cache.js
+++ api/video/clean_cache.js
@@ -14,12 +14,28 @@ finder.on('link', function (link, stat) {
     
 });
 finder.on('end', function (file, stat) {
-     let goalsize = 100000000;
+     
      list = list.sort(function(a, b) {
           return (new Date(a.mtime) > new Date(b.mtime))? 1 : -1;
      });
      let clean_list= [];
+     let goalsize = 1000000000;
+     
+     _f['diskspace'] = function(cbk) {
+          var diskspace = require(env.root_path + '/package/diskspace/node_modules/diskspace');
+          diskspace.check('/', function (err, space) {
+              space.total = Math.round(space.total);
+              space.free = space.free;
+              space.free_rate =  Math.floor(space.free  * 100 /  space.total); 
+              space.channel = channel.channel;
+              cbk(space);
+          });	
+     };     
      for (var i = 0; i < list.length; i++) {
+          if  (CP.data.space.free < goalsize) {
+          
+          
+          }
           if ((goalsize - list[i].size) > 0) {
                goalsize -= list[i].size;
                clean_list.push(list[i].fn);
@@ -32,21 +48,16 @@ finder.on('end', function (file, stat) {
      });
 });
 
-
 var batchDelete = function(list, cbk) {
      let CP = new pkg.crowdProcess();
      let _f = {}, fn = []; 
      for (var i = 0; i < list.length; i++) {
           _f['P_'+i] = (function(i) {
                return function(cbk1) {
-                    pkg.fs.unlink(list[i],function(err){
-                         // if(err) return console.log(err);
+                  //  pkg.fs.unlink(list[i],function(err){
                          cbk1('deleted ' + list[i]);
-                    });                   
-                    
-               
-               }
-               
+                  //  });
+               } 
           })(i);
      }
      CP.serial(
