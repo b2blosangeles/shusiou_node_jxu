3ae5f0d44ddc08394a2255c779af74789fe91cd4
b2blosangeles <b2blosangeles@gmail.com>
2018-03-14 22:39:18 -0700

    Update clean_cache.js

--- api/video/clean_cache.js
+++ api/video/clean_cache.js
@@ -25,7 +25,8 @@ finder.on('end', function (file, stat) {
                clean_list.push(list[i].fn);
           } 
      }
-  //   res.send(list.length + '---' + clean_list.length);
+     res.send(list.length + '---' + clean_list.length);
+     return true;
      batchDelete(clean_list, function(data) {
           res.send(data);    
      });
